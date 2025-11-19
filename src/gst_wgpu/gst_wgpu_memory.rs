ffe602e6e78c71ab9c1aaeb53bb2322dc3a1dec7
SpeedCrash100 <SpeedCrash100@yandex.ru>
2025-11-19 10:46:59 +0300

    refactor: disable gst_wgpu for now

--- src/gst_wgpu/gst_wgpu_memory.rs
+++ src/gst_wgpu/gst_wgpu_memory.rs
@@ -1,21 +1,37 @@
 mod imp;
 
+use std::mem::MaybeUninit;
+
 use gst::{
-    glib::{object::Cast, translate::ToGlibPtr},
+    glib::{object::Cast, subclass::types::ObjectSubclassIsExt, translate::ToGlibPtr},
+    mini_object_wrapper,
     prelude::GstObjectExt,
 };
 
-use crate::glib;
+use crate::{glib, gst_wgpu::WgpuContext};
 
 pub const GST_CAPS_FEATURE_MEMORY_WGPU: &str = "memory:wgpu";
+
+#[repr(transparent)]
+pub struct WgpuMemory(imp::WgpuMemory);
+
+impl WgpuMemory {}
 
 glib::wrapper! {
     pub struct WgpuMemoryAllocator(ObjectSubclass<imp::WgpuMemoryAllocator>) @extends gst::Allocator, gst::Object;
 }
 
 impl WgpuMemoryAllocator {
-    pub fn new() -> Self {
-        let mut out: WgpuMemoryAllocator = glib::Object::new();
+    /// Crates an allocator that uses specified context for allocating buffers
+    pub fn new(context: WgpuContext) -> Self {
+        let out: Self = glib::Object::new();
+
+        let imp = out.imp();
+        // SAFETY: We set context one time, it does not mutate after creation
+        // The creation itself cannot be parallel to be a problem
+        unsafe {
+            *imp.context.get() = Some(context);
+        };
 
         let allocator = out.upcast_ref::<gst::Allocator>();
         let allocator_stash: glib::translate::Stash<
@@ -29,9 +45,36 @@ impl WgpuMemoryAllocator {
         // SAFETY: ptr valid as long as allocator_stash is alive
         let allocator_pointer: &mut gst::ffi::GstAllocator = unsafe { &mut *raw_alloc_pointer };
 
-        allocator_pointer.mem_map_full = Some(imp::gst_wgpu_memory_map_full);
+        // todo!("Set functions");
 
-        todo!("Set functions");
         out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use gst::subclass::prelude::AllocatorImpl;
+
+    use super::*;
+    use crate::gst_wgpu::{gst_wgpu_memory::WgpuMemoryAllocator, WgpuContext};
+
+    #[test]
+    fn alloc() {
+        let context = WgpuContext::new(
+            &wgpu::RequestAdapterOptions {
+                compatible_surface: None,
+                ..Default::default()
+            },
+            &wgpu::DeviceDescriptor {
+                ..Default::default()
+            },
+            true,
+        );
+
+        let allocator = WgpuMemoryAllocator::new(context);
+        let mem = allocator.imp().alloc(64, None).unwrap();
+
+        let mem_ptr = mem.as_mut_ptr();
+        assert!(unsafe { imp::gst_is_wgpu_memory(mem_ptr) });
     }
 }
--- src/gst_wgpu/gst_wgpu_memory/imp.rs
+++ src/gst_wgpu/gst_wgpu_memory/imp.rs
@@ -1,9 +1,13 @@
+use std::cell::UnsafeCell;
+use std::mem::MaybeUninit;
 use std::os::raw::c_void;
 use std::sync::LazyLock;
 
-use gst::glib::object::Cast;
+use glib::object::ObjectType;
+use gst::glib::object::{Cast, ObjectExt};
 use gst::glib::subclass::object::ObjectImpl;
-use gst::glib::translate::FromGlibPtrBorrow;
+use gst::glib::subclass::types::ObjectSubclassExt;
+use gst::glib::translate::{from_glib_full, FromGlibPtrBorrow, ToGlibPtr};
 use gst::glib::{subclass::types::ObjectSubclass, translate::from_glib};
 use gst::subclass::prelude::{AllocatorImpl, GstObjectImpl};
 use wgpu::{BufferView, BufferViewMut};
@@ -11,83 +15,15 @@ use wgpu::{BufferView, BufferViewMut};
 use crate::glib;
 use crate::gst_wgpu::{WgpuContext, CAT};
 
-enum MappedData {
-    Ref(BufferView),
-    Mut(BufferViewMut),
-}
-
-impl MappedData {
-    pub fn pointer(&self) -> *mut u8 {
-        match self {
-            MappedData::Ref(view) => view.as_ptr() as *mut u8,
-            MappedData::Mut(view) => view.as_ptr() as *mut u8,
-        }
-    }
-}
-
 #[repr(C)]
+#[derive(Debug)]
 pub struct WgpuMemory {
-    parent: gst::ffi::GstMemory,
+    pub(super) parent: gst::ffi::GstMemory,
     context: WgpuContext,
     buffer: wgpu::Buffer,
-
-    offset: usize,
-    size: usize,
 }
 
-#[repr(C)]
-pub struct WgpuMemoryAllocParams {
-    base: gst::ffi::GstAllocationParams,
-    context: WgpuContext,
-    size: usize,
-}
-
-unsafe extern "C" fn wgpu_memory_init(
-    memory: *mut WgpuMemory,
-    allocator: *mut gst::ffi::GstAllocator,
-    alloc_params: WgpuMemoryAllocParams,
-    parent: *mut gst::ffi::GstMemory,
-    context: WgpuContext,
-    usages: wgpu::BufferUsages,
-) {
-    assert!(!memory.is_null(), "memory is null");
-    assert!(memory.is_aligned(), "memory is not aligned");
-    assert!(!allocator.is_null(), "allocator is null");
-    assert!(allocator.is_aligned(), "allocator is not aligned");
-
-    let wgpu_alignment = wgpu::MAP_ALIGNMENT as usize - 1;
-
-    let flags = alloc_params.base.flags;
-    let align = alloc_params.base.align | wgpu_alignment;
-    let offset = alloc_params.base.prefix;
-    let maxsize = alloc_params.size + alloc_params.base.prefix + alloc_params.base.padding;
-    let alloc_size = maxsize + align;
-
-    gst::ffi::gst_memory_init(
-        memory as *mut gst::ffi::GstMemory,
-        flags,
-        allocator,
-        parent,
-        maxsize,
-        align,
-        offset,
-        alloc_params.size,
-    );
-
-    let mem = &mut *memory;
-    mem.context = context.clone();
-
-    mem.buffer = mem.context.device().create_buffer(&wgpu::BufferDescriptor {
-        label: None,
-        mapped_at_creation: false,
-        size: alloc_size as u64,
-        usage: usages,
-    });
-    mem.size = alloc_params.size;
-    mem.offset = alloc_params.base.prefix;
-}
-
-unsafe extern "C" fn gst_is_wgpu_memory(memory: *mut gst::ffi::GstMemory) -> bool {
+pub(super) unsafe extern "C" fn gst_is_wgpu_memory(memory: *mut gst::ffi::GstMemory) -> bool {
     let mem = unsafe { &*memory };
 
     if mem.allocator.is_null() {
@@ -102,77 +38,23 @@ unsafe extern "C" fn gst_is_wgpu_memory(memory: *mut gst::ffi::GstMemory) -> boo
     true
 }
 
-pub(super) unsafe extern "C" fn gst_wgpu_memory_map_full(
-    memory: *mut gst::ffi::GstMemory,
-    map_info: *mut gst::ffi::GstMapInfo,
-    _size: usize,
-) -> *mut c_void {
-    assert!(!memory.is_null(), "memory is null");
-    assert!(!map_info.is_null(), "map_info is null");
-
-    let downcased_mem: *mut WgpuMemory = unsafe { core::mem::transmute(memory) };
-    let mem = unsafe { &mut *downcased_mem };
-
-    let map_info = unsafe { &mut *map_info };
-
-    let (tx, rx) = std::sync::mpsc::sync_channel(0);
-
-    let mode = if map_info.flags & gst::ffi::GST_MAP_READ != 0 {
-        wgpu::MapMode::Read
-    } else if map_info.flags & gst::ffi::GST_MAP_WRITE != 0 {
-        wgpu::MapMode::Write
-    } else {
-        gst::error!(CAT, "unsupported flags: {}", map_info.flags);
-        return core::ptr::null_mut();
-    };
-    // todo: impl GST_MAP_REF_MEMORY
-
-    if mem.size < map_info.maxsize {
-        gst::error!(CAT, "buffer is too small");
-        return core::ptr::null_mut();
-    }
-
-    let start = mem.offset;
-    let end = start + mem.size;
-    let bounds = (start as u64)..(end as u64);
-
-    mem.buffer.map_async(mode, bounds.clone(), move |res| {
-        tx.send(res).ok();
-    });
-
-    match rx.recv() {
-        Ok(Ok(_)) => {}
-        Ok(Err(err)) => {
-            gst::error!(CAT, "map error: {}", err);
-            return core::ptr::null_mut();
-        }
-        Err(_) => {
-            gst::error!(CAT, "map closed");
-            return core::ptr::null_mut();
-        }
-    };
-
-    // Now we can get range
-    let mapped_data = match mode {
-        wgpu::MapMode::Read => MappedData::Ref(mem.buffer.get_mapped_range(bounds)),
-        wgpu::MapMode::Write => MappedData::Mut(mem.buffer.get_mapped_range_mut(bounds)),
-    };
-
-    let user_data = Box::new(mapped_data);
-    let data = user_data.pointer();
-    map_info.data = data;
-    map_info.user_data = [
-        Box::into_raw(user_data) as *mut c_void,
-        core::ptr::null_mut(),
-        core::ptr::null_mut(),
-        core::ptr::null_mut(),
-    ];
-    map_info.size = mem.size;
-
-    data as *mut c_void
+#[derive(Debug)]
+pub struct WgpuMemoryAllocator {
+    pub(super) context: UnsafeCell<Option<WgpuContext>>,
 }
 
-pub struct WgpuMemoryAllocator;
+impl WgpuMemoryAllocator {
+    #[inline]
+    fn context(&self) -> &WgpuContext {
+        let ctx = unsafe { &*self.context.get() };
+        ctx.as_ref().unwrap()
+    }
+
+    #[inline]
+    fn device(&self) -> &wgpu::Device {
+        self.context().device()
+    }
+}
 
 #[glib::object_subclass]
 impl ObjectSubclass for WgpuMemoryAllocator {
@@ -181,10 +63,99 @@ impl ObjectSubclass for WgpuMemoryAllocator {
     type ParentType = gst::Allocator;
 
     fn with_class(_class: &Self::Class) -> Self {
-        Self
+        Self {
+            context: Default::default(),
+        }
     }
 }
 
 impl ObjectImpl for WgpuMemoryAllocator {}
 impl GstObjectImpl for WgpuMemoryAllocator {}
-impl AllocatorImpl for WgpuMemoryAllocator {}
+impl AllocatorImpl for WgpuMemoryAllocator {
+    fn alloc(
+        &self,
+        size: usize,
+        params: Option<&gst::AllocationParams>,
+    ) -> Result<gst::Memory, glib::BoolError> {
+        let allocator_obj = self.obj().clone().upcast::<gst::Allocator>();
+
+        let mut base_mem = MaybeUninit::<gst::ffi::GstMemory>::zeroed();
+
+        let mut align = wgpu::MAP_ALIGNMENT as usize - 1;
+        let mut offset = 0;
+        let mut maxsize = size;
+        let mut flags = 0;
+
+        if let Some(p) = params {
+            flags = p.flags().bits();
+            align |= p.align();
+            offset = p.prefix();
+            maxsize += p.prefix() + p.padding();
+            // If we're add align bytes, we can align map as requested
+            maxsize += align;
+        }
+
+        let gst_mem_ptr = base_mem.as_mut_ptr() as *mut gst::ffi::GstMemory;
+        let gst_allocator_ptr =
+            allocator_obj.as_object_ref().to_glib_full() as *mut gst::ffi::GstAllocator;
+
+        unsafe {
+            gst::ffi::gst_memory_init(
+                gst_mem_ptr,
+                flags,
+                gst_allocator_ptr,
+                core::ptr::null_mut(),
+                maxsize,
+                align,
+                offset,
+                size,
+            )
+        };
+
+        let base_mem = unsafe { base_mem.assume_init() };
+
+        let mem_flags = gst::MemoryFlags::from_bits_truncate(flags);
+        let usages = if mem_flags.contains(gst::MemoryFlags::READONLY) {
+            wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ
+        } else {
+            wgpu::BufferUsages::COPY_SRC | wgpu::BufferUsages::MAP_WRITE
+        };
+
+        let wgpu_buffer = self.device().create_buffer(&wgpu::BufferDescriptor {
+            label: None,
+            mapped_at_creation: false,
+            size: maxsize as u64,
+            usage: usages,
+        });
+
+        let mem = Box::new(WgpuMemory {
+            parent: base_mem,
+            buffer: wgpu_buffer,
+            context: self.context().clone(),
+        });
+
+        let mem_ptr = Box::leak(mem) as *mut WgpuMemory; // This is a memory
+        let out_mem = unsafe { gst::Memory::from_glib_full(mem_ptr as *mut gst::ffi::GstMemory) };
+
+        Ok(out_mem)
+    }
+
+    fn free(&self, memory: gst::Memory) {
+        // Don't forget to unref allocator here, we're not static
+        let mem_ptr = memory.as_mut_ptr();
+        if !unsafe { gst_is_wgpu_memory(mem_ptr) } {
+            return;
+        }
+
+        let mem_raw = unsafe { &mut *mem_ptr };
+        mem_raw.allocator = core::ptr::null_mut();
+        let _alloc: gst::Allocator = unsafe { from_glib_full(mem_raw.allocator) };
+
+        // let wgpu_mem_ptr = mem_ptr as *mut WgpuMemory;
+        // let wgpu_mem = unsafe { &mut *wgpu_mem_ptr };
+        // wgpu_mem.buffer.destroy();
+    }
+}
+
+unsafe impl Send for WgpuMemoryAllocator {}
+unsafe impl Sync for WgpuMemoryAllocator {}
